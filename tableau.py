9d53fddce87c960ee8709e7f66a036b990601d41
Ben Catania <bjc288@cornell.edu>
2021-03-07 10:20:53 -0500

    made interface

--- tableau.py
+++ tableau.py
@@ -14,11 +14,7 @@ def partialRec(largeList):
         file_name=str(len(largeList[0][0])) + "_by_"+str(len(largeList[0][0][0]))
 
         with open('%s.txt' % file_name, "wb") as fp:
-            pickle.dump(largeList, fp)
-
-        with open('%s.txt' % file_name, "rb") as fp:   # Unpickling
-            b = pickle.load(fp)
-        print(b[0])
+            pickle.dump(largeList[:len(largeList)-1], fp)
 
         return largeList[:len(largeList)-1]
     else:
@@ -92,15 +88,84 @@ def partialOrder(matrix):
 
 #Creates the matrix and calls partialRec
 if __name__ == "__main__":
-    numRow = int(input("Please enter the number of rows of the tableau "))
-    numCol = int(input("Please enter the number of columns of the tableau "))
-    matrix = [[0] * numCol for i in range (numRow)]
-    for j in range (numCol):
-        for i in range (numRow):
-            #matrix[i][j] = int(input("Please enter entry " + "T" + str(j*numRow + i+1) + " "))
-            matrix[i][j] = j*numRow + i+1
-
-    list_mat = partialRec([[matrix]])
 
 
-#    print(list_mat)
+
+
+
+
+
+
+
+
+
+
+    print("")
+    print("Welcome to Tableau!\n")
+    print("")
+
+    print("Enter !discover to add a new tableau to the database")
+    print("Enter !research to learn about a tableau in the database")
+    print("Enter !quit to end the program\n")
+
+    option=""
+    while (option != "!quit"):
+        print("")
+        print("")
+        option = input("Select an option: ")
+        print("")
+        if(option=="!research"):
+
+            rows = input("Rows of Tableau: ")
+            columns = input("Columns of Tableau ")
+
+            file_name=str(rows) + "_by_"+str(columns)
+            try:
+
+                with open('%s.txt' % file_name, "rb") as fp:   # Unpickling
+                    matrix = pickle.load(fp)
+            except:
+                print("")
+                print("This tableau is not in database")
+                continue
+
+
+
+
+
+            print("Enter !print to print the matrix")
+            print("Enter !level to find a print a level of the matrix")
+            print("Enter !lookup to find the location of a matrix in the partial order")
+            print("Enter !length to find the depth of the partial order")
+            print("Enter !depth to find the depth of the partial order")
+            print("Enter !quit to end program\n")
+
+
+            option=input("Select an option: ")
+
+
+
+            if option == "!level":
+                level = input("Select a level: ")
+                print(matrix[int(level)-1])
+            elif (option == "!depth"):
+                print(len(matrix))
+            elif (option == "!length"):
+                print(matrixLength(matrix))
+            elif (option=="!lookup"):
+                print("Not yet implemented")
+            elif (option=="!print"):
+                print(matrix)
+
+        elif (option =="!discover"):
+
+            numRow = int(input("Please enter the number of rows of the tableau "))
+            numCol = int(input("Please enter the number of columns of the tableau "))
+            matrix = [[0] * numCol for i in range (numRow)]
+            for j in range (numCol):
+                for i in range (numRow):
+                    matrix[i][j] = j*numRow + i+1
+
+            list_mat = partialRec([[matrix]])
+
+    print("Thank you for using Tableau!\n")
